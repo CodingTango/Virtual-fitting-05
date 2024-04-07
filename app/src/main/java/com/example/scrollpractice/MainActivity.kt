a5030bd6ff7f71ca02d62baa213b0fff5456afbf
Yunho Choi <zldbsghzl@naver.com>
2024-04-07 16:30:06 +0900

    충돌해결 후 병합

--- app/src/main/java/com/example/scrollpractice/MainActivity.kt
+++ app/src/main/java/com/example/scrollpractice/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -87,7 +86,7 @@ fun ClothList(clothList: List<Clothes>, modifier: Modifier = Modifier) {
                 )
             }
             item {
-                Button(onClick = { /*TODO*/ }) {
+                Button(onClick = { /*define later*/ }) {
                     Text(stringResource(R.string.fitting_button))
                 }
             }
